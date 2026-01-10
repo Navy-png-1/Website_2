52b945c741f5861bc4dea7d02c103f15501ff1ba
Navy-png-1 <navythewriter09@gmail.com>
2026-01-10 19:59:57 +0000

    install tailwind website bootstrap

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return render_template("main.html")
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+app.run(debug=True)
