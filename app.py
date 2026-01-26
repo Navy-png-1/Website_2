57f11a012df2de66fb8ccb706a85ccc8fd3483bf
Navy-png-1 <navythewriter09@gmail.com>
2026-01-26 03:07:56 +0000

    text home page design

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("main.html")
+@app.route("/books")
+def books():
+    return render_template("books.html")
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+@app.route("/archive")
+def archive():
+    return render_template("archive.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 app.run(debug=True)
