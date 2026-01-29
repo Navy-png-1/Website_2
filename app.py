e0faf033afa384b1662ab48645f9b2e885836feb
Navy-png-1 <navythewriter09@gmail.com>
2026-01-29 16:10:03 +0000

    added page, books, archive, and projects, need to work more on

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 def main():
     return render_template("main.html")
+
 @app.route("/books")
 def books():
     return render_template("books.html")
+
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+
 @app.route("/archive")
 def archive():
     return render_template("archive.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/one")
+def one():
+    return render_template("one.html")
+
 app.run(debug=True)
